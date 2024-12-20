775bdf90e3558f7c242ebedd1cf76cc9b8b0b887
ProUzzal <uzzalahmed0001@gmail.com>
2024-12-20 16:40:01 +0600

    added files

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import Layout from "./components/layout/Layout";
+
+function App() {
+  return (
+    <Layout>
+      <section className="container py-20">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam,
+          repellendus libero quas porro vitae debitis voluptatum autem fugit
+          dolores nam facilis ullam recusandae nobis eum ipsum. Hic aut tempora,
+          molestias dignissimos nulla molestiae quae consequuntur modi quasi
+          totam adipisci!
+        </p>
+      </section>
+    </Layout>
+  );
+}
+
+export default App;
